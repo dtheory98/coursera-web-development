4e285bbf8cf3dea48dc9a32b02c23b20f3a6d092
Dhruv Chauhan <c_dhruv@ibab.in>
2020-10-24 19:39:20 +0530

    version 1

---
+++ mod4_solution/js/script.js
@@ -0,0 +1,12 @@
+(function () {
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "jim"];
+  for (var i = 0; i < names.length; i++) {
+    var firstLetter = names[i].charAt(0).toLowerCase();
+    if (firstLetter === 'j') {
+      byeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i]);
+    }
+  }
+
+})();
